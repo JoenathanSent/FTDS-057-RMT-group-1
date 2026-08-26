02a4eb69f0ed8933310313b1dcc2143aadb0b2e5
diditaditya <di2t.aditya@gmail.com>
2026-08-26 13:37:13 +0700

    add auto arima model and prediction

--- predict_price.py
+++ predict_price.py
@@ -1,3 +1,6 @@
+import warnings
+warnings.filterwarnings('ignore')
+
 import streamlit as st
 import seaborn as sns
 import pandas as pd
@@ -15,6 +18,10 @@ pangoly_price['date'] = pd.to_datetime(pangoly_price['date'])
 local_price = local_price.dropna(subset=['pangoly_group'])
 
 lgbm = joblib.load('./models/lgbm/lgbm.joblib')
+
+arima_cpu = joblib.load('./models/arima/cpu.joblib')
+arima_gpu = joblib.load('./models/arima/gpu.joblib')
+arima_ram = joblib.load('./models/arima/ram.joblib')
 
 def run_predict_price():
     st.title('DealSense')
@@ -43,19 +50,63 @@ def run_predict_price():
     q3 = selected_df[price_col].quantile(0.75)
     median = selected_df[price_col].quantile(0.5)
 
-    st.header("Harga")
+    st.header("Harga Saat Ini")
     lower_limit = f'{q1:,.0f}'.replace(',', '.')
     upper_limit = f'{q3:,.0f}'.replace(',', '.')
-    st.write(f'Rentang Harga: Rp. {lower_limit} - Rp. {upper_limit}')
+    st.write(f'Rentang Harga: Rp. {lower_limit} - Rp. {upper_limit} dari {len(selected_df)} data')
 
     group_opt = selected_df['pangoly_group'].iloc[0]
 
-    preds = predict(60, group_opt, pangoly_price, lgbm)
+    changes = []
 
+    # lightGBM
+    lgbm_pct_change = lgbm_predict(group_opt)
+    if lgbm_pct_change is not None:
+        changes.append(lgbm_pct_change)
+
+    # ARIMA
+    arima_pct_change = arima_predict(cat_opt, group_opt)
+    if arima_pct_change is not None:
+        changes.append(arima_pct_change)
+
+    pct_change = sum(changes)/len(changes)
+
+    st.header("Perkiraan Perubahan Harga")
+    st.write(f'Prediksi perubahan harga dalam 2 bulan ke depan:  {pct_change:.2f}% {"⬆" if pct_change > 0 else "⬇"}')
+
+def lgbm_predict(group_opt, steps = 60):
+    preds = predict(steps, group_opt, pangoly_price, lgbm)
+    
     last_historical_price = pangoly_price[pangoly_price['product'] == group_opt].iloc[-1]['avg']
     predicted_price = preds.iloc[-1]['avg']
     change = predicted_price - last_historical_price
     pct_change = (change/last_historical_price) * 100
 
-    st.write(f'Prediksi perubahan harga dalam 2 bulan ke depan: {pct_change:.2f}%')
+    return pct_change
 
+def arima_predict(cat_opt, group_opt, steps = 60):
+    model_group = None
+    if cat_opt == 'PROCESSORS':
+        model_group = arima_cpu
+    elif cat_opt == 'GRAPHICS CARDS':
+        model_group = arima_gpu
+    elif cat_opt == 'MEMORY':
+        model_group = arima_ram
+    else:
+        return None
+
+    if group_opt not in model_group:
+        return None
+
+    model = model_group[group_opt]
+    preds = model.predict(n_periods=steps)
+
+    last_price = pangoly_price[pangoly_price['product'] == group_opt].iloc[-1]['avg']
+    pred_price = preds.iloc[-1]
+    change = pred_price - last_price
+    pct_change = (change/last_price)*100
+
+    return pct_change
+
+if __name__ == '__main__':
+    run_predict_price()
