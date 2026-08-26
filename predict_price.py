7575409fd0befa81ebd0cfe339f44add783d7fad
diditaditya <di2t.aditya@gmail.com>
2026-08-26 13:51:39 +0700

    add date input

--- predict_price.py
+++ predict_price.py
@@ -6,6 +6,7 @@ import seaborn as sns
 import pandas as pd
 from pathlib import Path
 import joblib
+import datetime as dt
 
 from models.lgbm.predict import predict
 
@@ -57,22 +58,29 @@ def run_predict_price():
 
     group_opt = selected_df['pangoly_group'].iloc[0]
 
+
+    st.header("Perkiraan Perubahan Harga")
+
+    pred_date = st.date_input("Tanggal:", min_value='today', value=dt.datetime.now() + dt.timedelta(days=7))
+
+    steps = (pred_date - dt.datetime.now().date()).days
+
     changes = []
 
     # lightGBM
-    lgbm_pct_change = lgbm_predict(group_opt)
+    lgbm_pct_change = lgbm_predict(group_opt, steps=steps)
     if lgbm_pct_change is not None:
         changes.append(lgbm_pct_change)
 
     # ARIMA
-    arima_pct_change = arima_predict(cat_opt, group_opt)
+    arima_pct_change = arima_predict(cat_opt, group_opt, steps=steps)
     if arima_pct_change is not None:
         changes.append(arima_pct_change)
 
     pct_change = sum(changes)/len(changes)
 
-    st.header("Perkiraan Perubahan Harga")
-    st.write(f'Prediksi perubahan harga dalam 2 bulan ke depan:  {pct_change:.2f}% {"⬆" if pct_change > 0 else "⬇"}')
+    
+    st.write(f'Prediksi perubahan harga:  {pct_change:.2f}% {"⬆" if pct_change > 0 else "⬇"}')
 
 def lgbm_predict(group_opt, steps = 60):
     preds = predict(steps, group_opt, pangoly_price, lgbm)
